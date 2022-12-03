8955cf0628c1b13b7890b0d3f7742ca942dd8910
Rashmi <rashmichigon65@gmail.com>
2022-12-03 15:19:16 +0530

    Day16and17_DSAProgram

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Day16and17_DSAProgram
+{
+    internal class Program
+    {
+        static void permute(String s, String answer)
+        {
+            if (s.Length == 0)
+            {
+                Console.Write(answer + "  ");
+                return;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char ch = s[i];
+                String left_substr = s.Substring(0, i);
+                String right_substr = s.Substring(i + 1);
+                String rest = left_substr + right_substr;
+                permute(rest, answer + ch);
+            }
+        }
+        static void Main(string[] args)
+        {
+            String s = "ABC";
+            String answer = "";
+
+            Console.Write("\nAll possible strings are : ");
+            permute(s, answer);
+        }
+    }
+}
