0ffcff64df8c679c7aa2f9a6ed0366f55bf5de44
Rashmi <rashmichigon65@gmail.com>
2022-12-06 15:19:26 +0530

    MergeSort

--- Program.cs
+++ Program.cs
@@ -4,58 +4,31 @@ namespace Day16and17_DSAProgram
 {
     internal class Program
     {
-       
-        static void Main(string[] args)
+        
+    static void Main(String[] args)
+        
         {
-            int a, b, i, j, flag;
-
-            // Ask user to enter lower value of interval
-            Console.WriteLine("Enter lower bound of " +
-                              "the interval: ");
-
-            // Take input
-            a = int.Parse(Console.ReadLine());
-
-            // Ask user to enter upper value of interval
-            Console.WriteLine("\nEnter upper bound " +
-                              "of the interval: ");
-
-            // Take input
-            b = int.Parse(Console.ReadLine());
-
-            // Print display message
-            Console.WriteLine("\nPrime numbers between " +
-                              "{0} and {1} are: ", a, b);
-
-            // Traverse each number in the interval
-            // with the help of for loop
-            for (i = a; i <= b; i++)
-            {
-
-                // Skip 0 and 1 as they are
-                // neither prime nor composite
-                if (i == 1 || i == 0)
-                    continue;
-
-                // flag variable to tell
-                // if i is prime or not
-                flag = 1;
-
-                for (j = 2; j <= i / 2; ++j)
-                {
-                    if (i % j == 0)
-                    {
-                        flag = 0;
-                        break;
-                    }
-                }
-
-                // flag = 1 means i is prime
-                // and flag = 0 means i is not prime
-                if (flag == 1)
-                    Console.WriteLine(i);
-            }
+            int[] a = { 10, 29, 23, 6, 30, 15, 38, 40 };
+            int n = a.Length;
+            Console.Write("Before sorting array elements are - ");
+            printArray(a, n);
+            mergesort(a, 0, n - 1);
+            Console.Write("\nAfter sorting array elements are - ");
+            printArray(a, n);
         }
+
+        private static void mergesort(int[] a, int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void printArray(int[] a, int n)
+        {
+            throw new NotImplementedException();
+        }
+
+       
     }
-}
+    }
+
 
