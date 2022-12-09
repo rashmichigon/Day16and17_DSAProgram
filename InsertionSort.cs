a4baa08e953c5bb70db2370ba797b48b37311dc3
Rashmi <rashmichigon65@gmail.com>
2022-12-09 14:01:15 +0530

    InsertionSort

---
+++ InsertionSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16and17_DSAProgram
+{
+    public class InsertionSort
+    {
+        static void insert(int[] a) /* function to sort an aay with insertion sort */
+        {
+            int i, j, temp;
+            int n = a.Length;
+            for (i = 1; i < n; i++)
+            {
+                temp = a[i];
+                j = i - 1;
+
+                while (j >= 0 && temp <= a[j])  /* Move the elements greater than temp to one position ahead from their current position*/
+                {
+                    a[j + 1] = a[j];
+                    j = j - 1;
+                }
+                a[j + 1] = temp;
+            }
+        }
+
+        static void printArr(int[] a) /* function to print the array */
+        {
+            int i;
+            int n = a.Length;
+            for (i = 0; i < n; i++)
+                Console.Write(a[i] + " ");
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,31 +4,27 @@ namespace Day16and17_DSAProgram
 {
     internal class Program
     {
-        
-    static void Main(String[] args)
-        
+         static void Main()
+
         {
-            int[] a = { 10, 29, 23, 6, 30, 15, 38, 40 };
-            int n = a.Length;
-            Console.Write("Before sorting array elements are - ");
-            printArray(a, n);
-            mergesort(a, 0, n - 1);
-            Console.Write("\nAfter sorting array elements are - ");
-            printArray(a, n);
+            int[] a = { 98, 54, 53, 18, 21, 12 };
+            Console.Write("Before sorting array elements are - \n");
+            printArr(a);
+            insert(a);
+            Console.Write("\nAfter sorting array elements are - \n");
+            printArr(a);
         }
 
-        private static void mergesort(int[] a, int v1, int v2)
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void printArray(int[] a, int n)
-        {
-            throw new NotImplementedException();
-        }
-
-       
+    private static void printArr(int[] a)
+    {
+        throw new NotImplementedException();
     }
+
+    private static void insert(int[] a)
+    {
+        throw new NotImplementedException();
     }
+}
+}
 
 
